a0406332d7b0e3aed1727df317785dfb631fe06b
Rogerio Alves <rogerioalves.ee@gmail.com>
2022-08-17 22:33:50 -0300

    end first practice

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React, { useState } from "react";
+
+import AddUser from "./components/Users/AddUser";
+import UsersList from "./components/Users/UsersList";
+
+function App() {
+  const [usersList, setUsersList] = useState([]);
+
+  const addUserHandler = (uName, uAge) => {
+    // Pass a function to the set function when the
+    // change depends on the previous state
+    setUsersList((prevUsersList) => {
+      return [
+        ...prevUsersList,
+        { name: uName, age: uAge, id: Math.random().toString() },
+      ];
+    });
+  };
+  return (
+    <div>
+      <AddUser onAddUser={addUserHandler}></AddUser>
+      <UsersList users={usersList} />
+    </div>
+  );
+}
+
+export default App;
